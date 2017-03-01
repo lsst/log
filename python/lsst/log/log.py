97f2f94df0d9fbbd2ea2ba42f7ac415af42d7792
Pim Schellart <P.Schellart@princeton.edu>
2017-03-01 09:59:58 -0500

    Fix merge error

--- python/lsst/log/log.py
+++ python/lsst/log/log.py
@@ -26,55 +26,67 @@ import logging
 import inspect
 import os
 
+from lsst.utils import continueClass
+
 from ._log import Log
 
-TRACE = Log.TRACE
-DEBUG = Log.DEBUG
-INFO = Log.INFO
-WARN = Log.WARN
-ERROR = Log.ERROR
-FATAL = Log.FATAL
+TRACE = 5000
+DEBUG = 10000
+INFO = 20000
+WARN = 30000
+ERROR = 40000
+FATAL = 50000
 
-def Log_trace(self, fmt, *args):
-    self._log(Log.TRACE, fmt, *args)
-Log.trace = Log_trace
-del Log_trace
+@continueClass
+class Log:
+    def trace(self, fmt, *args):
+        self._log(Log.TRACE, False, fmt, *args)
 
-def Log_debug(self, fmt, *args):
-    self._log(Log.DEBUG, fmt, *args)
-Log.debug = Log_debug
-del Log_debug
+    def debug(self, fmt, *args):
+        self._log(Log.DEBUG, False, fmt, *args)
 
-def Log_info(self, fmt, *args):
-    self._log(Log.INFO, fmt, *args)
-Log.info = Log_info
-del Log_info
+    def info(self, fmt, *args):
+        self._log(Log.INFO, False, fmt, *args)
 
-def Log_warn(self, fmt, *args):
-    self._log(Log.WARN, fmt, *args)
-Log.warn = Log_warn
-del Log_warn
+    def warn(self, fmt, *args):
+        self._log(Log.WARN, False, fmt, *args)
 
-def Log_error(self, fmt, *args):
-    self._log(Log.ERROR, fmt, *args)
-Log.error = Log_error
-del Log_error
+    def error(self, fmt, *args):
+        self._log(Log.ERROR, False, fmt, *args)
 
-def Log_fatal(self, fmt, *args):
-    self._log(Log.FATAL, fmt, *args)
-Log.fatal = Log_fatal
-del Log_fatal
+    def fatal(self, fmt, *args):
+        self._log(Log.FATAL, False, fmt, *args)
 
-def Log__log(self, level, fmt, *args):
-    if self.isEnabledFor(level):
-        frame = inspect.currentframe().f_back    # calling method
-        frame = frame.f_back    # original log location
-        filename=os.path.split(frame.f_code.co_filename)[1]
-        funcname=inspect.stack()[2][3]
-        msg=fmt % args if args else fmt
-        self.logMsg(level, filename, funcname, frame.f_lineno, msg)
-Log._log = Log__log
-del Log__log
+    def tracef(self, fmt, *args, **kwargs):
+        self._log(Log.TRACE, True, fmt, *args, **kwargs)
+
+    def debugf(self, fmt, *args, **kwargs):
+        self._log(Log.DEBUG, True, fmt, *args, **kwargs)
+
+    def infof(self, fmt, *args, **kwargs):
+        self._log(Log.INFO, True, fmt, *args, **kwargs)
+
+    def warnf(self, fmt, *args, **kwargs):
+        self._log(Log.WARN, True, fmt, *args, **kwargs)
+
+    def errorf(self, fmt, *args, **kwargs):
+        self._log(Log.ERROR, True, fmt, *args, **kwargs)
+
+    def fatalf(self, fmt, *args, **kwargs):
+        self._log(Log.FATAL, True, fmt, *args, **kwargs)
+
+    def _log(self, level, use_format, fmt, *args, **kwargs):
+        if self.isEnabledFor(level):
+            frame = inspect.currentframe().f_back    # calling method
+            frame = frame.f_back    # original log location
+            filename = os.path.split(frame.f_code.co_filename)[1]
+            funcname = inspect.stack()[2][3]
+            if use_format:
+                msg = fmt.format(*args, **kwargs) if args or kwargs else fmt
+            else:
+                msg = fmt % args if args else fmt
+            self.logMsg(level, filename, funcname, frame.f_lineno, msg)
+
 
 # Export static functions from Log class to module namespace
 
