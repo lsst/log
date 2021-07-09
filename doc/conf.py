50c70a7fd3e19c8a5024b7379d71a4fc71965b5a
Andy Salnikov <salnikov@slac.stanford.edu>
2021-07-09 13:48:37 -0500

    One more check of generated documentation

--- doc/conf.py
+++ doc/conf.py
@@ -1,13 +1,12 @@
 """Sphinx configuration file for an LSST stack package.
 
-This configuration only affects single-package Sphinx documenation builds.
+This configuration only affects single-package Sphinx documentation builds.
 """
 
-from documenteer.sphinxconfig.stackconf import build_package_configs
-import lsst.log
+from documenteer.conf.pipelinespkg import *  # noqa: F403, import *
 
-
-_g = globals()
-_g.update(build_package_configs(
-    project_name='log',
-    version=lsst.log.version.__version__))
+project = "log"
+html_theme_options["logotext"] = project     # noqa: F405, unknown name
+html_title = project
+html_short_title = project
+doxylink = {}
